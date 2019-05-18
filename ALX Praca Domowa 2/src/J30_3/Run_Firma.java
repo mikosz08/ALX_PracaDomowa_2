dd65ce7560a2ca97dd6d674f8f18aea2c0aec18d
Mikolaj <46966211+mikosz08@users.noreply.github.com>
2019-05-18 18:00:23 +0200

    README
    Opis działania

--- ALX Praca Domowa 2/src/J30_3/Run_Firma.java
+++ ALX Praca Domowa 2/src/J30_3/Run_Firma.java
@@ -1,5 +1,9 @@
 package J30_3;
-
+/* 
+ *Program składa się z 2 menu: Menu Kursów i Menu Kursanta
+ * w pierwszym można utworzyć/wyswietlic/wyedytowac kurs i przypisać kursanta do kursu ('K') 
+ * kursanta tworzymy w drugim menu (wchodzimy do niego literką 'P')
+ * */
 public class Run_Firma {
 
 	public static void main(String[] args) {
